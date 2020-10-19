a71e3aaa43037512db62324e64374f8efb1f9cc3
cpravetz <chris@pravetz.net>
2020-10-19 13:31:41 -0400

    Update publications.js

--- server/publications.js
+++ server/publications.js
@@ -4,7 +4,7 @@ import { check, Match } from 'meteor/check';
 import { publishComposite } from 'meteor/reywood:publish-composite';
 import { User } from 'meteor/socialize:user-model';
 
-import { LikesCollection } from '../common/common.js';
+import { ChecksCollection } from '../common/common.js';
 
 
 const optionsArgumentCheck = {
@@ -14,7 +14,7 @@ const optionsArgumentCheck = {
 };
 
 
-publishComposite('socialize.likesFor', function publishLikesFor(linkedObjectId, options = { limit: 10, sort: { createdAt: -1 } }) {
+publishComposite('socialize.checksFor', function publishChecksFor(linkedObjectId, options = { limit: 100, sort: { createdAt: -1 } }) {
     check(linkedObjectId, String);
     check(options, optionsArgumentCheck);
     if (this.userId) {
@@ -26,15 +26,42 @@ publishComposite('socialize.likesFor', function publishLikesFor(linkedObjectId, 
         if (!blockIds.includes(linkedObjectId)) {
             return {
                 find() {
-                    return LikesCollection.find({ linkedObjectId, userId: { $nin: blockIds } }, options);
+                    return ChecksCollection.find({ linkedObjectId, userId: { $nin: blockIds } }, options);
                 },
                 children: [
                     {
-                        find(like) {
-                            return Meteor.users.find({ _id: like.userId });
+                        find(check) {
+                            return Meteor.users.find({ _id: check.userId });
                         },
                     },
                 ],
+            };
+        }
+    }
+    return this.ready();
+});
+
+publishComposite('socialize.checksBy', function publishChecksBy(aUserId, options = { limit: 100, sort: { createdAt: -1 } }) {
+    check(aUserId, String);
+    check(options, optionsArgumentCheck);
+    if (this.userId) {
+        const currentUser = User.createEmpty(this.userId);
+        const blockedUserIds = currentUser.blockedUserIds();
+        const blockedByUserIds = currentUser.blockedByUserIds();
+        const blockIds = [...blockedUserIds, ...blockedByUserIds];
+
+        if (!blockIds.includes(aUserId)) {
+            return {
+                find() {
+                    return ChecksCollection.find({ userId: aUserId }, options);
+                },
+                children: [
+                    {
+                        find(check) {
+                            return Meteor.users.find({ _id: check.userId });
+                        },
+                    },
+                ]
             };
         }
     }
